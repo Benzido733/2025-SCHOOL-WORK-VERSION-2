f7f61acca38aa981d2e9af555b58e25659fac1b7
Benzido733 <owethumabeloane28@gmail.com>
2025-07-07 12:57:40 +0200

    Add files via upload

---
+++ ClientApp/src/clientapp/ClientApp.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clientapp;
+
+import za.ac.tut.client.ClietFrame;
+
+/**
+ *
+ * @author Owethu
+ */
+public class ClientApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new ClietFrame();
+    }
+    
+}
